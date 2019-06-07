12b9eb547a5c95bca36c7798e7a5f6b54f9db312
JoeyMBrown <joey.ma.brown@gmail.com>
2019-06-06 21:16:31 -0400

    Revert "Added user_id to task table, created a sign_up route, fixed improper naming conventions in user.ex"
    This reverts commit 1c12df51ab4fe3402808a0790525fa4d4392f7ba.

--- priv/repo/migrations/20190603162354_add_user_id_to_todotasks.exs
+++
@@ -1,9 +0,0 @@
-defmodule HabiticaBackend.Repo.Migrations.AddUserIdToTodotasks do
-  use Ecto.Migration
-
-  def change do
-    alter table(:todo_tasks) do
-      add :user_id, references(:users)
-    end
-  end
-end
